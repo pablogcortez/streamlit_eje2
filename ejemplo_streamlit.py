5e0bbb117de5e8818082ad0273e9c2e1c0076fe8
pablogcortez <84234416+pablogcortez@users.noreply.github.com>
2024-02-27 15:34:49 -0300

    Update ejemplo_streamlit.py

--- ejemplo_streamlit.py
+++ ejemplo_streamlit.py
@@ -1,2 +1,28 @@
 import streamlit as st
-import pandas as pd
+import streamlit_authenticator as stauth
+import yaml
+
+# Step 2: Load configuration file and create authentication object
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+# Step 2: Render login widget
+authenticator.login()
+
+# Step 3: Authenticating users
+if st.session_state["authentication_status"]:
+    authenticator.logout()  # Optionally logout user
+    st.write(f'Welcome {st.session_state["name"]}')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
